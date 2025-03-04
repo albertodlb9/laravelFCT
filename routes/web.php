7b68511d63abdc7bb8036e3c968a0e6d7ae5b855
Alberto de la Blanca <saloalberto97@gmail.com>
2025-03-04 19:39:47 +0100

    arreglado fallo update company

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::middleware('role:admin,teacher')->group(function () {
     Route::get('companies/create', [CompanyController::class, 'create'])->name('companies.create');
     Route::post('companies', [CompanyController::class, 'store'])->name('companies.store');
     Route::get('companies/{company}/edit', [CompanyController::class, 'edit'])->name('companies.edit');
-    Route::patch('companies/{company}', [CompanyController::class, 'update'])->name('companies.update');
+    Route::put('companies/{company}', [CompanyController::class, 'update'])->name('companies.update');
     Route::delete('companies/{company}', [CompanyController::class, 'destroy'])->name('companies.destroy');
     Route::get('companies/{company}', [CompanyController::class, 'show'])->name('companies.show');
 });
